6f8df7e6a979ea542bd6efd6eea0d5db88b753be
Enea <enealila@outlook.com>
2018-04-16 16:58:49 +0200

    changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 //get homepage
 router.get('/',function(req,res){
-    res.render('index');
+    res.render('login.ejs');
 });
-
+router.get('/login',function(req,res){
+    res.render('login.ejs');
+});
+router.get('/index',function(req,res){
+    res.render('index.ejs');
+});
+router.get('/register',function(req,res){
+    res.render('register.ejs');
+});
 module.exports =router;
--- routes/users.js
+++ routes/users.js
@@ -1,51 +1,122 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var bcrypt = require('bcryptjs');
+var LocalStrategy = require('passport-local').Strategy;
+
 var User = require('../models/user');
-//register
-router.get('/register',function(req,res){
-    res.render('register');
+
+// Register
+router.get('/register', function(req, res){
+	res.render('register.ejs');
 });
 
-//login
-router.get('/login',function(req,res){
-    res.render('login');
+// index
+router.get('/index', function(req, res){
+	res.render('index.ejs');
 });
 
-//register user
-router.post('/register',function(req,res){
-    var name = req.body.name;
-    var email = req.body.email;
-    var username = req.body.username;            
-    var password = req.body.password;
-    var password2 = req.body.password2;
-    
-    req.checkBody('name', 'Name is required').notEmpty();
-    req.checkBody('email', 'Email is required').notEmpty();
-    req.checkBody('email', 'Please enter a valid email').isEmail();
-    req.checkBody('username', 'Username is required').notEmpty();
-    req.checkBody('password', 'Password is required').notEmpty();
-    req.checkBody('password2', 'Confirm Password is required').notEmpty();
-    req.checkBody('password2', 'Confirm Password Must Matches With Password').equals(req.body.password);
-    var errors = req.validationErrors();
-    if(errors){
-        res.render('register',{
-            errors:errors
+// Login
+router.get('/login', function(req, res){
+	res.render('login.ejs');
+});
+
+// Register User
+router.post('/register', function(req, res){
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+	// Validation
+	req.checkBody('name', 'Name is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('register',{
+			errors:errors
+		});
+	} else {
+		var newUser = new User({
+			name: name,
+			email:email,
+			username: username,
+			password: password
+		});
+
+		User.createUser(newUser, function(err, user){
+			if(err) throw err;
+			console.log(user);
+		});
+
+		req.flash('success_msg', 'You are registered and can now login');
+
+		res.redirect('/users/login');
+	}
+});
+
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//    User.getUserByUsername(username, function(err, user){
+//    	if(err) throw err;
+//    	if(!user){
+//    		return done(null, false, {message: 'Unknown User'});
+//    	}
+//    	User.comparePassword(password, user.password, function(err, isMatch){
+//    		if(err) throw err;
+//    		if(isMatch){
+//    			return done(null, user);
+//    		} else {
+//    			return done(null, false, {message: 'Invalid password'});
+//    		}
+//    	});
+//    });
+//   }));
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+    //match username
+    let query = {username:username};
+    User.findOne(query,function(err, user){
+        if(err) throw err;
+        if(!user){
+            return done(null,false,{message:'No user'});
+        }
+
+        //match password
+        bcrypt.compare(password,user.password,function(err,isMatch){
+        if(err) throw err;
+        if(isMatch){
+            return done(null,user);
+        }else{
+            return done(null,false,{message:'Wrong pass'});
+        }
         });
-    } else {
-        var newUser = new  User({
-            name: name,
-            email: email,
-            username: username,
-            password: password
-        });
-    User.createUser(newUser,function(err,user){
-        if(err)  throw err;
-        console.log(user);
-    });
+    })
+    }));
 
-    req.flash('success_msg','You are registered');
-    res.redirect('/users/login');
-    }
-    
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
 });
-module.exports =router;
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+router.post('/login',
+  passport.authenticate('local', {successRedirect:'/users/index', failureRedirect:'/users/register'}),
+  function(req, res) {
+    res.redirect('/users/index');
+  });
+
+module.exports = router;
